ffd4f5360ef928ba29ec68d4d79cbcb7913d27d3
denis <d.zelikson@smstraffic.ru>
2022-10-02 19:52:12 +0300

    state components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
   <h1>Vue Course</h1>
-
+  <!-- передача строки! -->
+  <State number="123" @changeUserName="log" />
 </template>
+
+<script setup>
+  import State from '@/components/State.vue';
+
+  const log = d => console.log({emit: d});
+</script>
--- src/components/State.vue
+++ src/components/State.vue
@@ -1,4 +1,82 @@
 <template>
+  <div class="wrap">
+    <!-- данные -->
+    <p class="content">
+      <span class="text">{{ userName }}</span>
+      <span class="sub">данные</span>
+    </p>
 
+    <!-- props -->
+    <p class="content">
+      <span class="text">{{ number }}</span>
+      <span class="sub">props</span>
+    </p>
 
+    <!-- computed -->
+    <p class="content">
+      <span class="text">{{ userZip }}</span>
+      <span class="sub">computed</span>
+    </p>
+  </div>
 </template>
+
+<script>
+  export default {
+    emits: ['changeUserName'],
+    props: ['number'],
+    // проверка на тип
+    // props: {
+    //   number: {
+    //     type: Number,
+    //     required: true,
+    //     default: 111,
+    //     validator(arg){
+    //       if(arg > 10) return;
+    //       return arg;
+    //     }
+    //   }
+    // },
+    data(){
+      // доступ ко всем данным через this.$data
+      return {
+        userName: 'Alex',
+      }
+    },
+    computed: {
+      userZip(){
+        if(this.userName === 'Alex') return 123;
+        else return 321;
+      }
+    },
+    watch: {
+      userName: function(val, oldVal){
+        console.log(val);
+        console.log(oldVal);
+        this.$emit('changeUserName', val);
+      },
+      userZip: function(val, oldVal){
+        console.log(val);
+        console.log(oldVal);
+      }
+    },
+    methods: {
+      // нежелательное добавление данных
+      addDataAge(){
+        this.$data.age = 10;
+      },
+      async getData(){
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const json = await res.json();
+        return json;
+      },
+    },
+    async mounted(){
+      // this.addDataAge();
+      // console.log(this.$data);
+      // console.log(await this.getData());
+      setTimeout(() => {
+        this.userName = 'John';
+      }, 2000)
+    }
+  }
+</script>
