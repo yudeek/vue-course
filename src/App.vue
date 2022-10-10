11e7f9d91ca6573c480aa3b1db8e264007620031
denis <d.zelikson@smstraffic.ru>
2022-10-10 11:36:41 +0300

    provide inject

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <h1>Vue Course</h1>
   <!-- <ReactivityBasic /> -->
-  <Watching />
+  <!-- <Watching /> -->
+  <ProvideInject />
 </template>
 
 <script setup>
   import ReactivityBasic from '@/components/ReactivityBasic.vue';
   import Watching from '@/components/Watching.vue';
+  import ProvideInject from '@/components/ProvideInject.vue';
+
+  import { ref, provide } from 'vue'
+
+  const userCounter = ref(200);
+
+  provide('userValue', 100);
+  provide('userCounter', userCounter);
+
+  setTimeout(() => {
+    userCounter.value = 300;
+    // provide('userValue', 500); нельзя
+  }, 2000)
 
 
 </script>
---
+++ src/components/ProvideInject.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrap">
+    <!-- inject -->
+    <p class="content">
+      <span class="text">{{ userCounter }}</span>
+      <span class="sub">inject</span>
+    </p>
+
+    <!-- inject -->
+    <p class="content">
+      <span class="text">{{ userValue }}</span>
+      <span class="sub">inject</span>
+    </p>
+  </div>
+</template>
+
+<script setup>
+  import { inject } from 'vue';
+
+  const userCounter = inject('userCounter');
+  const userValue = inject('userValue');
+</script>
